ef39c02cd800724637777926487fe1a94d86b059
Raj Rai <rrai1590@gmail.com>
2021-10-23 18:30:45 +0530

    reversekelements

---
+++ dsa/Queue/Reversefirstkitemofqueue.cpp
@@ -0,0 +1,35 @@
+// Reverse first K item of Queue
+// i/p 10 20 30 40 50 60
+// o/p 30 20 10 40 50 60
+
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+int main(){
+  int n; cin>>n;
+  queue<int> q;
+  for(int i=0;i<n;i++){
+    int a; cin>>a;
+    q.push(a);
+  }
+  int k=3;
+  stack<int> s;
+  for(int i=0;i<k;i++){
+    s.push(q.front());
+    q.pop();
+  }
+  while(!s.empty()){
+    q.push(s.top());
+    s.pop();
+  }
+  for(int i=0;i<q.size()-k;i++){
+    q.push(q.front());
+    q.pop();
+  }
+  while(!q.empty()){
+    cout<<q.front()<<" ";
+    q.pop();
+  }
+  
+}
---
+++ dsa/Queue/generatenumberwithdigit.cpp
@@ -0,0 +1,22 @@
+// Given a number n print first n number such that all these numbers have digits in set {5,6}
+// add 5 and 6 to the queue then make a choice to append wither 5 or 6 tp the number
+
+#include<iostream>
+#include<queue>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n; cin>>n;
+  queue<string> q;
+  q.push("5");
+  q.push("6");
+  for(int i=0;i<n;i++){
+    string curr= q.front();
+    cout<<curr<<" ";
+    q.pop();
+    q.push(curr+"5");
+    q.push(curr+"6");
+  }
+  return 0;
+}
