c358b0a2c0d885e32869ff3fa45781628742515a
Raj Rai <rrai1590@gmail.com>
2021-10-27 23:23:22 +0530

    count distinct in every window

---
+++ dsa/unordered_map/count_distinct_in_every_window.cpp
@@ -0,0 +1,58 @@
+// Count Distinct element in every window
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+//  Naive Solution - count with O(n*k*k)
+
+int main()
+{
+  int v[] = {10, 10, 5, 3, 20, 5};
+  int k = 4, n = 6;
+  for (int i = 0; i <= n - k; i++)
+  {
+    int count = 0;
+    for (int j = 0; j < k; j++)
+    {
+      bool check = false;
+      for (int p = 0; p < j; p++)
+      {
+        if (v[i + j] == v[i + p])
+        {
+          check = true;
+          break;
+        }
+      }
+      if (check == false)
+        count++;
+    }
+    cout << count << " ";
+  }
+  return 0;
+}
+
+// Efficient Solution - Hashing with O(n)
+
+  // Freq Map of first K elements
+  // Print size of freq Map
+  // For(int i=k;i<n;i++)
+    // Decrease freq of a[i-k]
+    // if the freq of a[i-k]=0, delete it
+    // if a[i] does not exist in map insert it, else increase freq
+    // Print size of map
+
+int main(){
+  int v[] = {10, 10, 5, 3, 20, 5};
+  int k = 4, n = 6;
+  unordered_map<int,int> m;
+  for(int i=0;i<k;i++)
+    m[v[i]]++;
+  cout<<m.size()<<" ";
+  for(int i=k;i<n;i++){
+    m[v[i-k]]--;
+    if(m[v[i-k]]==0) m.erase(v[i-k]);
+    m[v[i]]++;
+  }  
+}
---
+++ dsa/unordered_map/winnerofelection.cpp
@@ -0,0 +1,27 @@
+// Given votes in election, find the winner
+
+#include<iostream>
+#include<unordered_map>
+#include<vector> 
+using namespace std;
+
+// Naive approach- count with O(n^2*max_len)
+
+int main(){
+  vector<string> v={"abc", "xyz", "abc", "xyz"};
+  unordered_map<string, int> m;
+  for(auto i: v)
+    m[i]++;
+  int freq=0;
+  string winner="";
+  for(auto i: m){
+    if(i.second>freq){
+      freq=i.second;
+      winner=i.first;
+    }
+    else if(i.second==freq && i.first<winner){
+      winner=i.first;
+    }
+  }
+  cout<<winner;
+}
