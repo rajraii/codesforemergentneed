b4e30f036962fe012c5ff8d47a5f03ace5e54160
Raj Rai <rrai1590@gmail.com>
2021-10-27 17:10:13 +0530

    check for pair

---
+++ dsa/unordered_set/checkforpair.cpp
@@ -0,0 +1,30 @@
+// Print pair sum in unsorted array
+
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+
+// Naive approach with o(n^2)
+
+int main(){
+  int n=5;
+  int a[n]={1,1,2,3,4};
+  int sum=5;
+  for(int i=0;i<n;i++){
+    int A,b;
+    bool check=false;
+    for(int j=0;j<n;j++){
+      if(a[i]+a[j]==sum) {
+        check=true;
+        A=a[i];
+        b=a[j];
+        break;
+      }
+    }
+    if(check=true) {
+      cout<<A<<" "<<b;
+      break;
+    }
+  }
+  return 0;
+}
