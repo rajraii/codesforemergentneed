7823bbd2852f8041bd79cfc76e5192deae2f7ac4
Raj Rai <rrai1590@gmail.com>
2021-11-13 19:53:58 +0530

    count divisible pair in arrray

---
+++ dsa/Hashing/Count_divisble_pair_array.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int main(){
+  int a[]={2,3,5,7};
+  int n=4;
+  int count=0;
+  for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+      if(a[j]%a[i]==0 || a[i]%a[j]==0) count++;
+    }
+  }
+  cout<<count<<endl;
+}
+
+// Efficiet approach Hashing O(nlogn)
+
+// create a hash table 
+// find the maximum
+//  find the multiple of element and if there are increment count
+
+int main(){
+  int a[]={1,2,3,6,5,7,8};
+  int n=7;
+  unordered_map<int,int> b;
+  int mx=0;
+  for(int i=0;i<n;i++) {
+    b[a[i]]++;
+    if(a[i]>mx) mx=a[i];
+  }
+  // cout<<mx<<endl;
+  int count=0;
+  for(int i=0;i<n;i++){
+    int j=2;
+    int temp=a[i];
+    while(temp*j<=mx){
+      if(b.find(temp*j)!=b.end()) {
+        // j++;
+        count++;
+      }
+      j++;
+    }
+  }
+  cout<<count<<endl;
+}
