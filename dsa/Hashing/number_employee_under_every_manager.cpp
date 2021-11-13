5b01e7b3ec30fc9278d940907ab5b261f4beed59
Raj Rai <rrai1590@gmail.com>
2021-11-13 18:59:38 +0530

    number employee under one manaer.cpp

---
+++ dsa/Hashing/number_employee_under_every_manager.cpp
@@ -0,0 +1,9 @@
+// given a dictionary that contains mapping of employee and his manager as a number of (employee, manager) pairs like below.
+// Write a function to get no of employees under each manager in the hierarchy not just their direct reports. It may be assumed that an employee directly reports to only one manager. In the above dictionary the root node/ceo is listed as reporting to himself.
+// Output should be a Dictionary that contains following.
+
+#include<iostream>
+using namespace std;
+int main(){
+  
+}
