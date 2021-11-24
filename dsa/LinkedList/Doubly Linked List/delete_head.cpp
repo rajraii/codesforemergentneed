53fb8e94eaa03ab6fb8d48908315c61e9d523927
Raj Rai <rrai1590@gmail.com>
2021-11-24 21:26:04 +0530

    delete the head of a doubly linkedlist

--- dsa/LinkedList/Doubly Linked List/delete_head.cpp
+++ dsa/LinkedList/Doubly Linked List/delete_head.cpp
@@ -1,1 +1,33 @@
-// 
+// Delete head of linkedlist
+
+#include<iostream>
+using namespace std;
+
+class node
+{
+public:
+  int data;
+  node *next;
+  node *prev;
+  node(int x)
+  {
+    data = x;
+    next = NULL;
+    prev = NULL;
+  }
+};
+
+// ! Delet head of Linkedlist 
+
+node* delet(node *head){
+  if(head==NULL) return NULL;
+  if(head->next==NULL){
+    delete head;
+    return NULL;
+  }
+  node* temp= head;
+  head=head->next;
+  head->prev=NULL;
+  delete temp;
+  return head;
+}
