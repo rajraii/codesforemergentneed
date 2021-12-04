f7a83b942273a992987589a8eeef1947660aafef
Raj Rai <rrai1590@gmail.com>
2021-12-04 20:46:06 +0530

    rempve duplicate from sorted linked list

---
+++ dsa/LinkedList/Questions/remove_duplicate.cpp
@@ -0,0 +1,24 @@
+// Remove duplicates from Sorted linkedlist
+
+#include<iostream>
+using namespace std;
+
+struct node
+{
+  int data;
+  node *next; 
+};
+
+
+// ! Method -1
+void remove(node *head){
+  node *curr=head;
+  while(curr != NULL && curr->next != NULL){
+    if(curr->data==curr->next->data){
+      node *temp=curr->next;
+      curr->next=curr->next->next;
+      delete temp;
+    }
+    else curr=curr->next;
+  }
+}
