c7c7cd2c730b3c03d225df87a48aa045aad88dbe
Raj Rai <rrai1590@gmail.com>
2021-12-08 22:19:37 +0530

    clone LL with random operator

--- dsa/LinkedList/Questions/cone_ll_with_random.cpp
+++ dsa/LinkedList/Questions/cone_ll_with_random.cpp
@@ -51,11 +51,7 @@ node *clone(node *head){
   step 2:  copy the random pointer of the cloned node with th eoriginal node
   step 3:  break the nodes to get the original Linked list
 */
-node *push(node *head, int x){
-  node *temp=new node (x);
-  temp->next=head;
-  return temp;
-}
+
 
 void display(node *head){
   while(head->next != NULL){
@@ -65,16 +61,55 @@ void display(node *head){
 }
 
 node *Clone(node *head){
-  node *curr=head;
+  node *curr=head, *temp;
   while(curr != NULL){
-    node *next=curr->next;
+    temp=curr->next;
     curr->next=new node(curr->data);
-    curr->next->next=next;
-    curr=next;
+    curr->next->next=temp;
+    curr=temp;
   }
   for(curr=head; curr != NULL; curr=curr->next->next)
     curr->next->random=(curr->random->next)? curr->random->next : NULL;
-  
-  
 
+  node *orgnl=head, *copy=head->next;
+  temp = copy;
+  while(orgnl && copy){
+    orgnl->next= orgnl->next ? orgnl->next->next : orgnl->next;
+
+    copy->next= copy->next ? copy->next->next : copy->next;
+    orgnl=orgnl->next;
+    copy= copy->next;
+  }
+  return temp;
+}
+
+int main()
+{
+    node* start = new node(1);
+    start->next = new node(2);
+    start->next->next = new node(3);
+    start->next->next->next = new node(4);
+    start->next->next->next->next = new node(5);
+ 
+    // 1's random points to 3
+    start->random = start->next->next;
+ 
+    // 2's random points to 1
+    start->next->random = start;
+ 
+    // 3's and 4's random points to 5
+    start->next->next->random =start->next->next->next->next;
+    start->next->next->next->random =start->next->next->next->next;
+ 
+    // 5's random points to 2
+    start->next->next->next->next->random =start->next;
+ 
+    cout << "Original list : \n";
+    display(start);
+ 
+    cout << "\nCloned list : \n";
+    node *cloned= Clone(start);
+    display(cloned);
+ 
+    return 0;
 }
