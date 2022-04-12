27623aabd660f2c22c878cf41e6dca655e5b94d3
Raj Rai <rrai1590@gmail.com>
2022-04-12 18:56:19 +0530

    coin change problem combination

--- DSA_Pep/DP/7.coiChange.cpp
+++ DSA_Pep/DP/7.coiChange.cpp
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-int coinhange(vector<int> a, int target){
-  int dp[target+1]={0};
-  int dp[0]=1;
-  for(auto i : a){
 
-  }
-}
 int main(){
+  vector<int> v= {2,3,5,6};
+  int target= 7;
 
+  vector<int>dp(target+1);
+  dp[0]=1;
+  for( int i=0;i<v.size();i++){
+    for(int j=v[i]; j<dp.size();j++){
+      if(j>=v[i])
+        dp[j] += dp[j-v[i]];
+    }
+  }
+  cout<<dp[target];
 return 0;
 }
+
